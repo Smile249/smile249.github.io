31a946cead7a9577a50971f077b788901f01ca5f
Smile <85846415+Smile249@users.noreply.github.com>
2022-10-30 10:58:44 +0100

    Added commissioning section Added tooltip/title to images for cover art of most recent maps

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 fetch("https://api.beatsaver.com/maps/uploader/4284314/0")
   .then((res) => res.json())
   .then((map) => {
+
+    //get cover art for 4 most recent maps
     document.getElementById("coverOne").src =
       map.docs[0].versions[map.docs[0].versions.length - 1].coverURL;
     document.getElementById("coverTwo").src =
@@ -10,6 +12,7 @@ fetch("https://api.beatsaver.com/maps/uploader/4284314/0")
     document.getElementById("coverFour").src =
       map.docs[3].versions[map.docs[3].versions.length - 1].coverURL;
 
+      //create link for 4 most recent maps
     document.getElementById("IdOne").href =
     "https://beatsaver.com/maps/" + map.docs[0].id;
     document.getElementById("IdTwo").href =
@@ -18,4 +21,11 @@ fetch("https://api.beatsaver.com/maps/uploader/4284314/0")
       "https://beatsaver.com/maps/" + map.docs[2].id;
     document.getElementById("IdFour").href =
       "https://beatsaver.com/maps/" + map.docs[3].id;
+
+      //get name of 4 most recent maps
+      document.getElementById("coverOne").title = map.docs[0].name;
+      document.getElementById("coverTwo").title = map.docs[1].name;
+      document.getElementById("coverThree").title = map.docs[2].name;
+      document.getElementById("coverFour").title = map.docs[3].name;
+      
   });
